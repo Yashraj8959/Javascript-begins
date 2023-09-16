be8fe2d61306b4e91390a16c70b418ec04b42eb1
Yashraj kushwah <110288913+Yashraj8959@users.noreply.github.com>
2023-09-16 19:39:57 +0000

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict" // treat all JS code as a newer version
+
+// alert("hey") // we are using nodeJs not browser
+
+let name = "yashraj"
+let age = 20
+let isloggedIn = true
+let mood
+
+// number => 2 to power 53
+// bigint => large number
+// string => " "
+// boolean => true/false
+// null => standalone value  
+// undefined
+// symbol => unique
+
+console.log( typeof null) // object
+console.log(typeof undefined); // undefined
