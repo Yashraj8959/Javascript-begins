cecb2787182d24052e7415105fdc06cd36fe36f5
Yashraj kushwah <110288913+Yashraj8959@users.noreply.github.com>
2023-09-16 19:13:10 +0000

    variables

---
+++ 01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 12345
+let accountName = "yashraj"
+var accountEmail = "ykushwah7828@gmail.com"
+accountCity = "gwalior"
+let accountState
+
+console.table([accountId,accountName,accountEmail,accountCity,accountState])
+// acountId = 34324  Not allowed
+accountName = "kushwah"
+accountEmail = "yashraj@gmail.com"
+accountCity = "Indore"
+ 
+console.log(accountId);
+console.table([accountName,accountEmail,accountCity,accountState])
+
+/*
+prefer not to use var
+*/
