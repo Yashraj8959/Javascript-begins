4185ea8b8ddb2e5490ef8ae281e75521017a27e4
Yashraj kushwah <110288913+Yashraj8959@users.noreply.github.com>
2023-09-17 15:44:06 +0000

    operations

---
+++ 03_conversionOperation.js
@@ -0,0 +1,45 @@
+let number = 53
+// console.log(typeof number);
+let newNumber = String(number)
+// console.log(typeof newNumber)
+
+let abc = 1
+// console.log(typeof abc);
+let abc1 = String(abc)
+// console.log(typeof abc1);
+let abc2 = Boolean(abc1)
+// console.log(abc2)
+
+// ********************* Operations **********************
+
+let value = 4
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%3)
+
+let str1 = "Hello"
+let str2 = " Yashraj"
+let str3 = str1 + str2
+// console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3");
+
+console.log(+true);
+console.log(+"");
+
+let gamecounter =100
+++gamecounter
+console.log(gamecounter);
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
